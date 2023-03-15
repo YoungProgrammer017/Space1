7cf1e1761aa12aa1c732bedbeb4a6675a0ae4ff7
YoungProgrammer017 <125394458+YoungProgrammer017@users.noreply.github.com>
2023-03-15 09:52:31 +0400

    Add files via upload

---
+++ Space.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector('.btn')
+function btnFunc () {
+    document.location.href = 'button.html'
+}
+
+
+let headMenu = document.querySelector ('.headtext_menu')
+let head = document.querySelector ('.headtext')
+function widthfunc(width) {
+
+    if (width.matches) {
+        headMenu.style.display = 'block'
+        head.style.display = 'none'
+    } else {
+        headMenu.style.display = 'none'
+        head.style.display = 'flex'
+    }
+}
+
+let width = window.matchMedia("(max-width: 1000px)")
+
+widthfunc(width) 
+width.addListener(widthfunc)
